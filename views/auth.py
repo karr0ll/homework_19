4860fbf2ace4459c8fbb55559cb5f7458aea52e4
Kirill Tuchkin <volkirtu@gmail.com>
2023-02-27 10:41:53 +0300

    fixed return code 200

--- views/auth.py
+++ views/auth.py
@@ -18,13 +18,13 @@ class AuthsView(Resource):
             return "", 400
 
         tokens = auth_service.generate_tokens(username, password)
-        return tokens, 201
+        return tokens, 200
 
 
     def put(self):
         data = request.json
         token = data.get("refresh_token")
         tokens = auth_service.approve_refresh_token(token)
-        return tokens, 201
+        return tokens, 200
 
 
